442f9af5200b1c5ae839845c129e47ac425962f9
Juliano Lofy Junior <juliano.junior@havan.com.br>
2024-02-06 10:58:28 -0300

    Initial Commit

---
+++ MyPhotoApp/MyPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPhotoAppApp.swift
+//  MyPhotoApp
+//
+//  Created by Juliano Lofy Junior on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
